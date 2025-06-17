6ddcbdbd9910c573803c35c981e629db4a25b944
anirudh-som <167941251+anirudh-som@users.noreply.github.com>
2025-06-17 13:34:59 +0000

    Deploying to gh-pages from @ anirudh-som/anirudh-som.github.io@3d6b92d85881abbeecd49909a573e53ed08b653d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,28 +501,21 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%63%6F%6E%74%61%63%74.%61%6E%69%72%75%64%68.%73%6F%6D@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://www.linkedin.com/in/anirudhsom", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=dY2-WmIAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
