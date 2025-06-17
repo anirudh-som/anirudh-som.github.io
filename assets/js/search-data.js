65f06ef820df5c4456796e21acbc4657fa3623ca
anirudh-som <167941251+anirudh-som@users.noreply.github.com>
2025-06-17 15:41:50 +0000

    Deploying to gh-pages from @ anirudh-som/anirudh-som.github.io@58de6d292a1e1de513499f91295a537639652c32 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,8 +37,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/repositories/";
           },
-        },{id: "nav-cv",
-          title: "cv",
+        },{id: "nav-experience",
+          title: "Experience",
           description: "",
           section: "Navigation",
           handler: () => {
