0b0b065827985880ce252a3c474da08db9c87d74
anirudh-som <167941251+anirudh-som@users.noreply.github.com>
2025-06-18 13:32:10 +0000

    Deploying to gh-pages from @ anirudh-som/anirudh-som.github.io@d555591109ed4beedeaaa2540a68b7e0e914b18a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -14,7 +14,7 @@ ninja.data = [{
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/cv/";
+            window.location.href = "/about/";
           },
         },{id: "nav-publications",
           title: "Publications",
